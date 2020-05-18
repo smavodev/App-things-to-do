559fd084246fd5ee5e2e61dc2166ee89d800f5dc
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-05-18 18:58:46 -0500

    add createList, SaveStorage, LoadStorage /deseos.services

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,14 +9,51 @@ import { Router } from '@angular/router';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  
-  constructor( public _deseosService: DeseosService,
-                private _router:Router) {
-    
-  }
-  
-  addList(){
-    this._router.navigateByUrl('/tabs/tab1/agregar');
+
+  constructor(public _deseosService: DeseosService,
+    private _router: Router,
+    private _alertCtrl: AlertController) {
 
   }
+
+  async addList() {
+    //this._router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this._alertCtrl.create({
+      header: 'Nueva Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la Lista'
+        }
+      ],
+      buttons: [
+        {
+          text:'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text:'Crear',
+          handler: (data) => {
+            console.log(data);
+            if(data.titulo.length===0){
+              return;
+            }
+
+            this._deseosService.createList(data.titulo);
+
+            //Crear lista
+            
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Lista } from '../models/list.model';
+import { IfStmt } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,32 @@ export class DeseosService {
 
   constructor() { 
     //console.log('Servicio Inicializado');
-    const Lista1 = new Lista('Lista1');
-    const Lista2 = new Lista('Lista2');
+    this.LoadStorage();
+    
+    /*const Lista1 = new Lista('Lista1');
+    const Lista2 = new Lista('Lista2');*/
 
-    this.listas.push(Lista1,Lista2);
+    /*this.listas.push(Lista1,Lista2);*/
     //console.log(this.listas);
+  }
 
+  createList(titulo:string){
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.SaveStorage();
+  }
+
+  SaveStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas) );
+    console.log(this.listas);
+  }
+  
+  LoadStorage(){
+    if(localStorage.getItem('data') ){
+      this.listas = JSON.parse(localStorage.getItem('data') );
+    } else {
+      this.listas = [];
+    }
     
   }
 
